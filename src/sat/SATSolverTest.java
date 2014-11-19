8f49684f01a2485fae30963eaf767e4fcadb42d3
agrimsingh <agrim.singh@gmail.com>
2014-11-19 22:07:44 +0800

    new cnf + solver
    solver test update + testcases

--- src/sat/SATSolverTest.java
+++ src/sat/SATSolverTest.java
@@ -1,17 +1,26 @@
 package sat;
 
 import static org.junit.Assert.*;
+
 import java.io.BufferedReader;
-//import java.io.File;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.io.Writer;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
+
 import org.junit.Test;
+
 import sat.env.*;
 import sat.formula.*;
+
 
 public class SATSolverTest {
     Literal a = PosLiteral.make("a");
@@ -20,36 +29,41 @@ public class SATSolverTest {
     Literal na = a.getNegation();
     Literal nb = b.getNegation();
     Literal nc = c.getNegation();
+
+
     private static List<String> readFile(String filename)
     {
-    	List<String> records = new ArrayList<String>();
-    	try
-    	{
-	        BufferedReader reader = new BufferedReader(new FileReader(filename));
-	        String line;
-	        while ((line = reader.readLine()) != null)
-		        {
-		        	records.add(line);
-		        }
-	        reader.close();
-	        return records;
-    	}
-    	catch (Exception e)
-    	{
-	        System.err.format("Exception occurred trying to read '%s'.", filename);
-	        e.printStackTrace();
-	        return null;
-    	}
+      List<String> records = new ArrayList<String>();
+      try
+      {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+        while ((line = reader.readLine()) != null)
+        {
+          records.add(line);
+        }
+        reader.close();
+        return records;
+      }
+      catch (Exception e)
+      {
+        System.err.format("Exception occurred trying to read '%s'.", filename);
+        e.printStackTrace();
+        return null;
+      }
     }
-	
-	public static void main(String[] args) {
-		final String file = "C:\\Users\\Agrim\\Desktop\\ISTD\\50.001\\2d\\sampleCNF\\s8.cnf";
+
+	@Test
+	public void main(){
+//		final String file = "C:\\Users\\Dhrubajit\\Documents\\eclipse workspace\\2d\\testcases\\t6000-4000.cnf";
+		final String file = "C:\\Users\\Agrim\\Documents\\GitHub\\2dproject\\sampleCNF\\largeSat.cnf";
 		Formula f = new Formula();
-		try {
+		HashMap<String,Literal> lit = new HashMap<String,Literal>();
+		int var_num=-1;
+		int clause_num=-1;
+		try{
 			List<String> content = readFile(file);
 			List<Integer> list = new ArrayList<Integer>();
-			int var_num=-1;
-			int clause_num=-1;
 			for (String line:content){
 				if (!line.isEmpty()){ //ignoring empty lines
 					if(line.charAt(0)!='c'){ //ignoring comments
@@ -57,10 +71,10 @@ public class SATSolverTest {
 							if (var_num!=-1)
 								throw new Exception("Multiple problem lines specified");
 							String[] split = line.split(" ");
-							if (split.length!=4 || split[0]!="p"){
+							if (split.length!=4 || !split[0].equals("p")){
 								throw new Exception("Invalid problem line");
 							}
-							if (split[1]!="cnf"){
+							if (!split[1].toLowerCase().contains("cnf")){
 								throw new Exception("Invalid format specified");
 							}
 							var_num = Integer.parseInt(split[2]);
@@ -76,37 +90,80 @@ public class SATSolverTest {
 					}
 				}
 			}
-			HashMap<String,Literal> lit = new HashMap<String,Literal>();
-			for(int i=0;i<var_num;i++){
+			for(int i=1;i<=var_num;i++){
 				String name = new Integer(i).toString();
 				lit.put(name, PosLiteral.make(name));
 			}
 			
 			List<Literal> literal_in_clause = new ArrayList<Literal>();
-			for (int i:list){
+			System.out.println(list);
+			for (Integer i:list){
 				if(i==0){
-					f.addClause(makeCl((Literal[])literal_in_clause.toArray()));
-					literal_in_clause = new ArrayList<Literal>();
+					if (!literal_in_clause.isEmpty()){
+						Clause c=null;
+						try{
+							c = makeCl(literal_in_clause.toArray(new Literal[literal_in_clause.size()]));
+						} catch (Exception e){
+							System.out.println("Unable to create clause "+literal_in_clause.toString());
+						}
+						if (c!=null){
+							f = f.addClause(c);
+						} else {
+							f = f.addClause(new Clause());
+						}
+						literal_in_clause = new ArrayList<Literal>();
+					}
 				}
 				if (i>0){
 					literal_in_clause.add(lit.get(new Integer(i).toString()));
 				}
 				if (i<0){
-					literal_in_clause.add(lit.get(new Integer(i).toString()).getNegation());
+					literal_in_clause.add(lit.get(new Integer(-i).toString()).getNegation());
 				}
 			}
+			if (literal_in_clause.size()>0){ //add all remaining data
+				f = f.addClause(makeCl(literal_in_clause.toArray(new Literal[literal_in_clause.size()])));
+			}
 			if (f.getSize()!=clause_num){
-				throw new Exception("Clause number mismatch");
+				System.out.println("Warning: Clause number mismatch. Expected "+clause_num+". Found "+f.getSize()+".");
 			}
 		} catch (Exception e){
+			e.printStackTrace();
 			System.out.println(e.getMessage());
 		}
+
+		//execution time
+		System.out.println("SAT solver starts!!!");
+		long started = System.nanoTime(); 
 		Environment e = SATSolver.solve(f);
-		//asserts
+		long time = System.nanoTime();
+		long timeTaken= time - started;
+		System.out.println("Time:" + timeTaken/1000000.0 + "ms");
 		
+		
+		String x ="";
+		try{
+			for (int i=1;i<=var_num;i++){
+				//this will throw exception if unsatisfiable
+				x += String.valueOf(i)+": "+e.get(lit.get(new Integer(i).toString()).getVariable()).toString()+"\n";
+			}
+			System.out.println("satisfiable");
+			System.out.println(x);
+			try {
+		         File file2 = new File("C:\\Users\\Agrim\\Documents\\GitHub\\2dproject\\sampleCNF\\BoolAssignment.txt");
+		         BufferedWriter output = new BufferedWriter(new FileWriter(file2));
+		         output.write(x);
+		         output.close();
+			 }catch ( IOException e1 ) {
+		           e1.printStackTrace();
+		     }
+		}
+			
+		catch(NullPointerException exception){
+			System.out.println("unsatisfiable");
+		}
+	
 	}
-    
-	// TODO: put your test cases for SATSolver.solve here
 	
     @Test
     public void testSATSolver1(){
